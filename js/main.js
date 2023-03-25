c3670bb4a5626c28af95f2c156eb71c453d70c7d
Bonnie <bonnie.multimedia@gmail.com>
2023-03-25 18:08:22 -0300

    Actualizacion 25/03

--- js/main.js
+++ js/main.js
@@ -70,6 +70,31 @@
             },        
         }
     });
+
+    // Carousel Servicios
+    $(".belvedere-carousel-servicios").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            760:{
+                items:2
+            },
+            1080:{
+                items:3
+            },        
+        }
+    });
     
 })(jQuery);
 
